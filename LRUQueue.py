0e5cd848f523a4d71e42e90a8d2c6885430c9572
Justin Paul <jspaul94@gmail.com>
2015-04-27 17:22:18 -0500

    added fifo queue and reorganzied replacement algorithms

--- LRUQueue.py
+++ LRUQueue.py
@@ -1,73 +1,103 @@
 '''LRUQueue.py'''
 from Queue import PriorityQueue
+from Queue import Queue
 import time
 import calendar
-
+from abc import ABCMeta
 '''class for least recently used algorithm'''
+
+class ReplacementQueue(object):
+
+    def enqueue(self, id):
+        pass 
+
+    def delete(self, id):
+        pass
+
+    def contains(self, id):
+        pass
+
 class PriorityQueueContain(PriorityQueue):
 
-	
+    def __delete__(self, id):
+	 with self.mutex:
+            for tuple in self.queue:
+                if(id == tuple[1]):
+                    self.queue.remove(tuple) 
+                    return True
+            return False
+
+class QueueContain(Queue):
+
     def __contains__(self, id):
         with self.mutex:
-            for node in self.queue:
-                if(id == node.getId()):
-                    node.updateTime()
+            for temp in self.queue:
+                if(temp == id):
                     return True
             return False
 
-    def __remove__(self, id):
-	 with self.mutex:
-            for node in self.queue:
-                if(id == node.getId()):
-                    self.queue.remove(node) 
-                    return True
-            return False
 
-            itemLRU = -1
-
-            if(len(self.queue) == self.maxsize):
-                print "blah"
-                item = self.get() 
-                itemLRU = item.getId()
-            newNode = Node(id)
-            self.put(newNode)
-            print id
-            return itemLRU
-
+    def __delete__(self, id):
+        with self.mutex:
+           for temp in self.queue:
+               if(id == temp):
+                   self.queue.remove(temp) 
+                   return True
+           return False
 
 
 class LRUQueue:
 	
     def __init__(self, maxsize):
         self.queue = PriorityQueueContain(maxsize)
-	
-    def enqueueLRU(self, id):
-        itemLRU = -1
 
+    def enqueue(self, id):
+        itemLRU = -1
         if(self.queue.full()):
             itemLRU = self.queue.get() 
         self.queue.put((int(time.time()*100000), id))
-
-        print id
         return itemLRU
 
     def contains(self, id):
-	for tuple in self.queue.queue:
-           if(tuple[1] == id):
-               return True
-        return False
+        isContained = self.delete(id)
+        if(isContained):
+            self.enqueue(id)
+        return isContained
  
-    def remove(self, id):
-	return self.queue.__remove__(id)
+    def delete(self, id):
+	return self.queue.__delete__(id)
+
+
+class FIFOQueue(ReplacementQueue):
+
+    def __init__(self, maxsize):
+        self.queue = QueueContain(maxsize)
+
+    def enqueue(self, id):
+        itemLRU = -1
+        if(self.queue.full()):
+            itemLRU = self.queue.get() 
+        self.queue.put(id)
+        return itemLRU
+
+    def contains(self, id):
+        return self.queue.__contains__(id)
+
+    def delete(self, id):
+        return self.queue.__delete__(id)
 
 '''Used for Testing Purposes'''
 def main():
-    queue = LRUQueue(5)
+    queue = FIFOQueue(5)
     for i in range(1, 20):
 	print i
-        queue.enqueueLRU(i)
-    for i in range(1, 20):
+        queue.enqueue(i)
+    queue.contains(15)
+    queue.enqueue(20)
+    for i in range(1, 21):
         print i, queue.contains(i)
+    print queue.delete(20)
+    print queue.contains(20)
 
 if __name__ == '__main__':
 	main()
